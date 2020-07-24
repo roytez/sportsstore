737cdf36c368edd38e8e65d96daf39b49ec04f3b
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-24 10:11:41 +0200

    Add controller higher level

---
+++ controllers/sportsStore.js
@@ -0,0 +1,13 @@
+angular.module("sportsStore")
+.controller("sportsStoreCtrl", function ($scope) {
+    $scope.data = {
+        { name: "Produkt #1", description: "To jest produkt.",
+            category: "Kategoria #1", price: 100},
+        { name: "Produkt #2", description: "To jest produkt.",
+            category: "Kategoria #1", price: 110},
+        { name: "Produkt #3", description: "To jest produkt.",
+            category: "Kategoria #2", price: 210},
+        { name: "Produkt #4", description: "To jest produkt.",
+            category: "Kategoria #3", price: 202},
+    };
+})
