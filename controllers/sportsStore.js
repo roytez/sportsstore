ef3055bd87e7f92bb1707ce36df057e9031fe886
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-27 16:01:28 +0200

    connect with deployd

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,18 +1,13 @@
 angular.module("sportsStore")
-.controller("sportsStoreCtrl", function ($scope) {
-    $scope.data = {
-        products: [
-            { name: "Produkt #1", description: "To jest produkt.",
-                category: "Kategoria #1", price: 100
-                },
-            { name: "Produkt #2", description: "To jest produkt.",
-                category: "Kategoria #1", price: 110
-                },
-            { name: "Produkt #3", description: "To jest produkt.",
-                category: "Kategoria #2", price: 210
-                },
-            { name: "Produkt #4", description: "To jest produkt.",
-                category: "Kategoria #3", price: 202
-                }]
-    };
-});
+    .constant("dataUrl", "http://localhost:5500/products")
+    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+        $scope.data = {};
+
+        $http.get(dataUrl)
+            .success(function (data) {
+                $scope.data.products = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+    });
