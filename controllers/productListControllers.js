79148b6f78d991e9a7cf1617a824f767ac4258b2
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-27 15:15:12 +0200

    add pagination

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,13 +1,32 @@
 angular.module("sportsStore")
-    .controller("productListCtrl", function ($scope, $filter) {
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCont", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCont) {
         var selectedCategory = null;
 
-        $scope.selectedCategory = function (newCategory) {
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCont;
+
+        $scope.selectCategory = function (newCategory) {
             selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectedPage = function (newPage) {
+            $scope.selectedPage = newPage;
         }
 
         $scope.categoryFilterFn = function (product) {
             return selectedCategory == null ||
                 product.category == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : "";
+        }
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
         }
     });
