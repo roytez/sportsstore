a2a79c2cfbb5a787fc9b76f9a91d7470841fcd22
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-29 12:36:51 +0200

    finalization of the order

--- controllers/checkoutControllers.js
+++ controllers/checkoutControllers.js
@@ -1,5 +1,5 @@
 angular.module("sportsStore")
-.controller("cartSummaryController", function($scope, cart) {
+.controller("cartSummaryController", function ($scope, cart) {
     $scope.cartData = cart.getProducts();
 
     $scope.total = function () {
