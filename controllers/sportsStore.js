5276479bbcc4a8827484bc8c178f1ea9df056f87
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-29 14:51:23 +0200

    combining the orders database with the application

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,7 +1,10 @@
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:5500/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
-        $scope.data = {};
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location,
+        dataUrl, orderUrl, cart) {
+        $scope.data = {
+        };
 
         $http.get(dataUrl)
             .success(function (data) {
@@ -10,4 +13,18 @@ angular.module("sportsStore")
             .error(function (error) {
                 $scope.data.error = error;
             });
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                $scope.data.orderId = data.id;
+                cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete")
+                });
+        }
     });
