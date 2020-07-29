31bc7c7ff8cb923b79642fec6b291a1297f2f5c5
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-29 09:51:04 +0200

    functionality of adding products to the cart

--- components/cart/cart.js
+++ components/cart/cart.js
@@ -20,7 +20,7 @@ angular.module("cart", [])
         },
 
         removeProduct: function (id) {
-            for (var i =0; i < cartData.length; i++) {
+            for (var i = 0; i < cartData.length; i++) {
                 if (cartData[i].id == id) {
                     cartData.splice(i, 1);
                     break
@@ -32,4 +32,28 @@ angular.module("cart", [])
             return cartData;
         }
     }
+})
+.directive("cartSummary", function (cart) {
+    return {
+        restrict: "E",
+        templateUrl: "components/cart/cartSummary.html",
+        controller: function ($scope) {
+            var cartData =cart.getProducts();
+
+            $scope.total = function () {
+                var total = 0;
+                for (var i = 0; i < cartData.length; i++) {
+                    total += (cartData[i].price * cartData[i].count)
+                }
+                return total;
+            }
+            $scope.itemCount = function () {
+                var total = 0;
+                for (var i = 0; i < cartData.length; i++) {
+                    total += cartData[i].count
+                }
+                return total;
+            }
+        }
+    };
 });
