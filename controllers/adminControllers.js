3a6188e81a2789473bc433b4796a03f2ccaf46b9
roytez <mateuszkrasinkiewicz@gmail.com>
2020-07-31 15:29:22 +0200

    add controller allows to download orders

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,6 +1,6 @@
 angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
-.controller("authCtrl", function ($scope, $http, $location, authUrl) {
+.controller("authCtrl", function($scope, $http, $location, authUrl) {
     $scope.authenticate = function (user, pass) {
         $http.post(authUrl, {
             username: user,
@@ -24,6 +24,29 @@ angular.module("sportsStoreAdmin")
 
     $scope.getScreen = function () {
         return $scope.current == "Produkty"
-            ? "/views/adminProducts.html" : "views/adminOrders.html";
+            ? "/views/adminProducts.html" : "/views/adminOrders.html";
     };
+})
+.controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+    $http.get(ordersUrl, { withCredentials : true })
+        .success(function (data) {
+            $scope.orders = data;
+        })
+        .error(function (error) {
+            $scope.error = error;
+        });
+
+    $scope.selectedOrder;
+
+    $scope.selectedOrder = function(order) {
+        $scope.selectedOrder = order;
+    };
+
+    $scope.calcTotal = function(order) {
+        var total = 0;
+        for (var i = 0; i < order.products.length; i++) {
+            total += order.products[i].count * order.products[i].price;
+        }
+        return total;
+    }
 });
